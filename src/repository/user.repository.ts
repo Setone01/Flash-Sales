bdfbbb019804b6787b8eb3d31fdb61676c0c6ef3
koopman001 <femia@teflonhub.com>
2025-03-07 10:32:13 +0100

    code refactoring

--- src/repository/user.repository.ts
+++ src/repository/user.repository.ts
@@ -1,38 +1,28 @@
-import { UserModel } from "../database/models/user.model";
-import { UserI } from "../interface";
+import { UserModel } from "@src/database/models/user.model";
+import { UserI } from "@src/interface";
 
-export class UserRepository {
+class UserRepository {
+  /**
+   * Get user by email
+   */
+  async getUserByEmail(email: string): Promise<UserI | null> {
+    return await UserModel.findOne({ email });
+  }
+
+  /**
+   * Get user by ID
+   */
+  async getUserById(id: string): Promise<UserI | null> {
+    return await UserModel.findById(id);
+  }
+
+  /**
+   * Create a new user
+   */
   async createUser(userData: Partial<UserI>): Promise<UserI> {
     const user = new UserModel(userData);
     return await user.save();
   }
-
-  async getUserByEmail(email: string): Promise<UserI | null> {
-    return await UserModel.findOne({ email }).lean();
-  }
-
-  async getUserById(id: string): Promise<UserI | null> {
-    return await UserModel.findById(id).lean();
-  }
-
-  async updateUser(
-    id: string,
-    updateData: Partial<UserI>
-  ): Promise<UserI | null> {
-    const user = await UserModel.findById(id); // Fetch user document
-    if (!user) return null; // Return null if user is not found
-
-    // Update fields only if provided
-    if (updateData.username) user.username = updateData.username;
-    if (updateData.email) user.email = updateData.email;
-    if (updateData.password) user.password = updateData.password;
-    if (updateData.purchasedProducts)
-      user.purchasedProducts = updateData.purchasedProducts;
-
-    return await user.save(); // Save changes and return updated user
-  }
-
-  async deleteUser(id: string): Promise<UserI | null> {
-    return await UserModel.findByIdAndDelete(id).lean();
-  }
 }
+
+export default new UserRepository();
